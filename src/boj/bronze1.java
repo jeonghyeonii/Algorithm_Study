14cc0836369c1a7a39740f6b3079f5a60ca78448
jeonghye0nii <wjh1004w@naver.com>
2024-12-13 18:43:15 +0900

    second commit

--- src/boj/bronze1.java
+++ src/boj/bronze1.java
@@ -1,7 +1,17 @@
 package boj;
 
+import java.util.Scanner;
+
 public class bronze1 {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] a = new int[num];
+        for(int i = 0; i < a.length; i++){
+            a[i] = scanner.nextByte();
+        }
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
     }
 }
