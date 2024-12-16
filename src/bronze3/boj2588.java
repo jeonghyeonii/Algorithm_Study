71402e1d5c8451a6e2fc9e41ac9d13ab7bee623f
jeonghye0nii <wjh1004w@naver.com>
2024-12-16 21:01:06 +0900

    first commit

---
+++ src/bronze3/boj2588.java
@@ -0,0 +1,22 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class boj2588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result1 = a*(b%10);
+        int result2 = a*((b%100)/10);
+        int result3 = a*(b/100);
+        int result4 = a*b;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+    }
+}
---
+++ src/bronze3/boj2884.java
@@ -0,0 +1,23 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class boj2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if(m < 45){
+            h--;
+            m = (m - 45) + 60;
+
+            if(h < 0){
+                h = 23;
+            }
+            System.out.println(h + " " + m);
+        } else {
+            System.out.println(h + " " + (m - 45));
+        }
+    }
+}
